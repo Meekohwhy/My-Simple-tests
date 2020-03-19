f5c08f88c68941cf4ac54d729c57d272e62b339c
Meekohwhy <51291808+Meekohwhy@users.noreply.github.com>
2020-03-19 16:20:25 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const{Builder,By,Key,util} = require("selenium-webdriver");
-async function example (){
-    let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://google.com"); //Pobranie adresu
-    await driver.findElement(By.name("q")).sendKeys("selenium",Key.RETURN);
-}
-example();
